a16c364724f2f246197bcbff0f11617a94c6779d
rivando-al-rasyid <nougat-tutorial@protonmail.com>
2026-03-09 11:18:42 +0700

    Feat : menghitung hasil perkalian dari semua array

---
+++ tasks/rivando/countArray.js
@@ -0,0 +1,48 @@
+// Sistem Kasir
+// const price = 250_000;
+// const money = 275_000;
+
+const cardinaNumber = [1,2,3,4,5,6,7,8,9,10]
+
+
+function countArray(arr){
+  let hasil = arr.forEach(number => number *= number )
+
+
+}
+
+
+constcountArray(cardinaNumber)
+
+// function myFunction(num) {
+//   return num * 10;
+// }
+
+
+
+
+
+// export function cashier(price, money) {
+//   if (typeof price == "number" && typeof money == "number") {
+//     const changes = {
+//       50: 0,
+//       10: 0,
+//       5: 0,
+//       2: 0,
+//       1: 0,
+//     };
+//     let diff = money - price;
+//     const nominals = Object.keys(changes);
+//     for (let i = nominals.length - 1; i >= 0; i--) {
+//       const nominal = parseInt(nominals[i]) * 1000;
+//       // console.log(nominal);
+//       if (diff >= nominal) {
+//         changes[nominals[i]] = Math.floor(diff / nominal);
+//         diff %= nominal;
+//       }
+//     }
+//     return changes;
+//   } else {
+//     throw new Error("INVALID");
+//   }
+// }
