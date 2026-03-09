87c72694f82a7148306dd95865d9204224b82863
l1mus <alinesezcore@gmail.com>
2026-03-09 11:32:06 +0700

    feat: export-import function manualBuilInFilter and manualBuilInMap from manualFnBuildInMethod.js to indext.js

---
+++ tasks/ali/index.js
@@ -0,0 +1,12 @@
+import {
+  manualBuilInFilter,
+  manualBuilInMap,
+} from "./manualFnBuildInMethod.js";
+import { cashier } from "./cashier.js";
+
+const func = {
+  manualBuilInFilter,
+  manualBuilInMap,
+};
+
+export default func;
