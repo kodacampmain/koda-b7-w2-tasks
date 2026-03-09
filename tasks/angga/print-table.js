34bd681637e58018c2b6d0b2e0ffa8dc8e3511be
anggavb <anggavb8@gmail.com>
2026-03-09 13:35:47 +0700

    fix: check parameter condition statement to one line

--- tasks/angga/print-table.js
+++ tasks/angga/print-table.js
@@ -1,6 +1,5 @@
 export const cetakTabel = (n) => {
-  if (typeof n !== "number") throw new Error("Parameter harus bertipe data nomor dan harus nomor positif");
-  if (n < 0) throw new Error("Parameter harus bertipe data nomor dan harus nomor positif");
+  if (typeof n !== "number" || n < 0) throw new Error("Parameter harus bertipe data nomor dan harus nomor positif");
 
   console.log("\n---- 2. Multiplication Table Program  ----");
   return [...Array(10)].map((v, i) => {
