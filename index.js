7f421297d380ee9d78e0341fc2ab4013d8caf8c2
Bernadus Dwiki Adicitra <dwiki.adicitra6@gmail.com>
2026-03-09 13:29:59 +0700

    feat: add new try catch for the new function

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
  */
 
 import fakhri from "./tasks/fakhri/index.js";
+import dwiki from "./tasks/dwiki/index.js";
 
 console.log("===========================================");
 // Use IIFE here
@@ -34,6 +35,55 @@ console.log("===========================================");
   try {
     const changes = fakhri.cashier("250_000", "375_000");
     console.log(changes);
+  } catch (error) {
+    console.log(error.message);
+  }
+  // Tugas 1 map dan filter dwiki
+  // Success
+  try {
+    const arrBaru = dwiki.manualMap([1,2,3,4,5]);
+    console.log(arrBaru);
+    const hasil = dwiki.manualFilter([12, 5, 8, 130, 44])
+    console.log(hasil)
+  } catch (error) {
+    console.log(error.message);
+  }
+  // Failed
+  try {
+    const arrBaru = dwiki.manualMap(1,2,3,4,5);
+    console.log(arrBaru);
+    const hasil = dwiki.manualFilter(12, 5, 8, 130, 44)
+    console.log(hasil)
+  } catch (error) {
+    console.log(error.message);
+  }
+  // Tugas 2 cetak tabel dwiki
+  // Success
+  try {
+    const hasil = dwiki.cetakTabel(10);
+    hasil
+  } catch (error) {
+    console.log(error.message);
+  }
+  // Failed
+  try {
+    const hasil = dwiki.cetakTabel("abc");
+    hasil
+  } catch (error) {
+    console.log(error.message);
+  }
+  // Tugas 3 count Array dwiki
+  // Succes
+  try {
+    const hasil = dwiki.countArray([1,2,3,4,5]);
+    console.log(hasil)
+  } catch (error) {
+    console.log(error.message);
+  }
+  // Failed
+  try {
+    const hasil = dwiki.countArray(1,2,3,4,5);
+    console.log(hasil)
   } catch (error) {
     console.log(error.message);
   }
