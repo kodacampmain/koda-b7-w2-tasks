01dcdc501f4298cee372f7af496f7c08b7d577ae
Bernadus Dwiki Adicitra <dwiki.adicitra6@gmail.com>
2026-03-09 13:27:22 +0700

    feat: add import and export function

---
+++ tasks/dwiki/index.js
@@ -0,0 +1,12 @@
+import { manualMap, manualFilter } from "./tugas1.js";
+import { cetakTabel } from "./tugas2.js";
+import { countArray } from "./tugas3.js";
+
+const func = {
+  manualMap,
+  manualFilter,
+  cetakTabel,
+  countArray
+};
+
+export default func;
