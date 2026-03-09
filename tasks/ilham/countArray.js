86f77d82ce3306bcb0b7974b33e99ca246087522
ilhammursidi <imursidi37@gmail.com>
2026-03-09 17:14:25 +0700

    style: fix indentation

--- tasks/ilham/countArray.js
+++ tasks/ilham/countArray.js
@@ -1,8 +1,8 @@
 export function countArray(arr) {
-  if(!(Array.isArray(arr))) {
-    throw new Error("Parameter harus berupa Array")
+  if (!Array.isArray(arr)) {
+    throw new Error("Parameter harus berupa Array");
   }
-    let hasil = 1;
+  let hasil = 1;
   for (let i = 0; i < arr.length; i++) {
     hasil *= arr[i];
   }
