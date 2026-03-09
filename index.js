0f25a42596035a46ea0c24ee2f65b3a9329bd016
ilhammursidi <imursidi37@gmail.com>
2026-03-09 16:43:50 +0700

    feat: add 4 function

--- index.js
+++ index.js
@@ -39,3 +39,79 @@ console.log("===========================================");
   }
 })();
 console.log("===========================================");
+/**
+ * Task Ilham
+ */
+
+import ilham from "./tasks/ilham/index.js";
+
+console.log("===========================================");
+(function taskIlham() {
+  try {
+    // manualMap
+    // success
+    const data = [1,2,3,4,5]
+    const tomanualMap = ilham.manualMap(data,(num)=>num*num);
+    console.log(tomanualMap)
+  } catch (error){
+    console.log(error.message)
+  }
+  try {
+    // error
+    const data = "2";
+    const toManualMap = ilham.manualMap(data,(num)=>num*num);
+    console.log(toManualMap)
+  } catch (error){
+    console.log(error.message)
+  }
+  try {
+    // manualFilter
+    // success
+    const data = [1,2,3,4,5];
+    const toManualFilter = ilham.manualFilter(data,(num)=>num % 2 === 0);
+    console.log(toManualFilter)
+  } catch(error) {
+    console.log(error.message)
+  }
+  try {
+    // Failed
+    const data = "1";
+    const toManualFilter = ilham.manualFilter(data,(num)=>num % 2 === 0);
+    console.log(toManualFilter)
+  } catch (error) {
+    console.log(error.message)
+  }
+  try {
+    // cetakTable
+    // success
+    const toTable = ilham.cetakTable(10)
+    console.log(toTable)
+  }catch (error) {
+    console.log(error.message)
+  }
+  try {
+    // failed
+    const tabel = ilham.cetakTable("abc")
+    console.log(tabel)
+  }catch (error){
+    console.log(error.message)
+  }
+  try {
+    // count Array
+    // success
+    const datanum = [1,2,3,4,5];
+    const toCount = ilham.countArray(datanum)
+    console.log(toCount)
+  }catch (error){
+    console.log(error.message)
+  } 
+  try {
+    // failed
+    const data = "abb";
+    const toCount = ilham.countArray(data)
+    console.log(toCount)
+  } catch(error) {
+    console.log(error.message)
+  }
+})()
+console.log("===========================================");
