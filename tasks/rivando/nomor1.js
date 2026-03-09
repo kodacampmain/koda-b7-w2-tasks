7917112dcbb8a9af3ad75e304ee7a7bfa7b75868
rivando-al-rasyid <nougat-tutorial@protonmail.com>
2026-03-09 13:01:06 +0700

    feat : membuat contoh lebih nyata

--- tasks/rivando/nomor1.js
+++ tasks/rivando/nomor1.js
@@ -1,6 +1,29 @@
-const numbers = [65, 44, 12, 4];
-const newArr = numbers.map(myFunction);
+const produk = [
+  { nama: 'Laptop', harga: 1000, stok: 5 },
+  { nama: 'Mouse', harga: 20, stok: 15 },
+  { nama: 'Keyboard', harga: 50, stok: 0 },
+  { nama: 'Monitor', harga: 300, stok: 8 }
+];
 
-function myFunction(num) {
-  return num  ;
+
+
+function cusArr(arr, callback) {
+  const hasil = [];
+  arr.forEach(element => {
+    if (callback(element)){
+      hasil.push(element)
+    }
+  });
+  return hasil;
+}
+
+function cusMap(arr, callback) {
+  const hasil = [];
+
+  arr.forEach(element => {
+    const itemTransformasi = callback(element);
+    hasil.push(itemTransformasi);
+  });
+
+  return hasil;
 }
